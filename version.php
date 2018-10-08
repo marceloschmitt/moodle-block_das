6883c7ee32d8e3769116f540b0b80c4ba5fd8352
Marcelo Augusto Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2018-10-08 20:47:41 -0300

    Filename change

--- version.php
+++ version.php
@@ -16,8 +16,8 @@
 
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2018100501;  // YYYYMMDDHH (year, month, day, 24-hr time).
+$plugin->version = 2018100502;  // YYYYMMDDHH (year, month, day, 24-hr time).
 $plugin->requires = 2015111600; // YYYYMMDDHH (This is the release version for Moodle 3.0).
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = 'v4.1.1';
+$plugin->release = 'v.0.2';
 $plugin->component = 'block_das';
