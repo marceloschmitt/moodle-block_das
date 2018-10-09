524349a49d37ae7db9eb96564b240a946d4f463c
patriciawalau <33928581+patriciawalau@users.noreply.github.com>
2018-10-09 17:47:54 -0300

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -448,7 +448,7 @@ include('../lib.php');
 
                 <div class="das-item-default-header">
                     <img class="das-user-small-image das-vertical-align" src="assets/img/rosto1.jpg" alt="User-Image">
-                    <p class="das-vertical-align das-p-overflow"><?php echo $nome ?></p>
+                    <p class="das-vertical-align das-p-overflow"><?php echo $nome; ?></p>
                     <img class="das-message-icon" src="assets/img/msg.png" alt="Message-Image">
                 </div>
                 <?php
