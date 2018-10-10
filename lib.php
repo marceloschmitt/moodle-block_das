61d9aafc639ea05d116718a987008547d7823722
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2018-10-10 10:08:18 -0300

    Update lib.php

--- lib.php
+++ lib.php
@@ -14,7 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-//defined('MOODLE_INTERNAL') || die();
+require_once("../../config.php");
+defined('MOODLE_INTERNAL') || die();
 
 function usuarios_online() {
 
