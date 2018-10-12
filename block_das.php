3a567f5527e03574d3265bd6a340ff22d760b6ac
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2018-10-12 09:28:51 -0300

    Update block_das.php

--- block_das.php
+++ block_das.php
@@ -39,7 +39,7 @@ class block_das extends block_base {
         $this->content = new stdClass;
         // $this->content->text = get_string('graphs', 'block_das');
         $this->content->text = "";
-        $this->content->text .= "<li> <a href= {$CFG->wwwroot}/blocks/das/dashboard/index.php 
+        $this->content->text .= "<li> <a href= {$CFG->wwwroot}/blocks/das/dashboard/index.php?id={$course->id} 
                           target=_blank>" . get_string('das', 'block_das') . "</a>";
       
         $this->content->footer = '<hr/>';
