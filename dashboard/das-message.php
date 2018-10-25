853f554009262c2c0dfb672153fda51bb9405157
patriciawalau <33928581+patriciawalau@users.noreply.github.com>
2018-10-25 15:14:12 -0300

    Add files via upload

---
+++ dashboard/das-message.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Caixa de Mensagem - DAS</title>
+	 <link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/das-style-message.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+</head>
+<body>
+	<div class="das-message">
+	
+		<div class="das-header">
+			<p class="das-title-message">Nome do Curso</p>
+			<p class="das-title-message">Nome do Bloco</p>
+			<p class="das-title-message">Nome do Tópico</p>
+			<p class="das-title-message">Nome da Atividade</p>			
+		</div>
+
+		<div class="das-text-area">
+			<p class="das-text">Escreva sua mensagem</p>
+		<textarea rows="10" cols="80"></textarea>
+		</div>
+
+	<div class="das-checkbox-users">
+		<p class="das-text"> <input type="checkbox" name="User1" value="user1"> Nome do Aluno 1</p></br>
+		<p class="das-text"> <input type="checkbox" name="User2" value="user2"> Nome do Aluno 2</p></br>
+		<p class="das-text"> <input type="checkbox" name="User3" value="user3"> Nome do Aluno 3</p></br>
+		<p class="das-text"> <input type="checkbox" name="User4" value="user4"> Nome do Aluno 4</p></br>
+		<p class="das-text"> <input type="checkbox" name="User5" value="user5"> Nome do Aluno 5</p></br>
+		<input type="checkbox" name="SelectAll" value="SelectAll">Selecionar todos</br>
+		<input type="checkbox" name="DeselectAll" value="DeselectAll">Deselecionar todos</br>
+
+		<input type="submit" name="Enviar">
+	</div>
+	
+	</div>
+</body>
+</html>
