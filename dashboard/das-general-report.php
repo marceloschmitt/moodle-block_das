462f120cb5c3500386a6e1c954e10ff7791d2ca3
patriciawalau <33928581+patriciawalau@users.noreply.github.com>
2018-10-30 17:21:42 -0300

    Add files via upload

---
+++ dashboard/das-general-report.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Das - Relatório Geral de Notas</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/das-general-report.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+<div class="das-general-report">	
+<p class="das-subtitle">Relatório Geral de notas do DAS</p>
+
+	<div class="das-filter-users">
+	<p>Selecionar filtro por:</p>
+		
+		<div class="das-select-topic">
+			<p>Tópico</p> 
+			<select name="das-select-topic-user">
+				<option>Selecionar...</option>
+				<option>Tópico 1</option>
+				<option>Tópico 2</option>
+				<option>Tópico 3</option>
+				<option>Tópico 4</option>
+			</select>
+		</div>
+
+		<div class="das-select-concept">
+			<p>Conceito</p>
+			<select>
+				<option>Selecionar...</option>
+				<option>A</option>
+				<option>B</option>
+				<option>C</option>
+				<option>D</option>
+				<option>FF</option>
+			</select>
+		
+		</div>
+
+		<div class="das-select-note">
+		<p>Nota</p> 
+			<select>
+				<option>Selecionar...</option>
+				<option>1</option>
+				<option>2</option>
+				<option>3</option>
+				<option>4</option>
+				<option>5</option>
+				<option>6</option>
+				<option>7</option>
+				<option>8</option>
+				<option>9</option>
+				<option>10</option>
+			</select>
+
+		</div>
+
+		<div class="das-select-period">
+		<p>Período</p>
+			<select>
+				<option>Selecionar...</option>
+				<option>1 dia</option>
+				<option>2 dias</option>
+				<option>3 dias</option>
+				<option>4 dias</option>
+				<option>5 dias</option>
+				<option>6 dias</option>
+				<option>1 semana</option>
+				<option>2 semanas</option>
+			</select>
+		</div>
+
+	</div>
+
+	<div class="das-table-select">
+		<table border="1" id="das-students-table">
+			<tr>
+				<td>Aluno</td>
+				<th colspan="4">Integração de Mídias na Educação </th>
+				<td>Conceito</td>
+			</tr>
+
+			<tr>
+				<td> </td>
+				<td>A1</td>
+				<td>A2</td>
+				<td>A3</td>
+				<td>A4</td>
+				<td> </td>
+			</tr>
+			
+			<tr>
+				<td>Anita Raquel da Silva</td>
+				<td>6</td>
+				<td>9</td>
+				<td>4</td>
+				<td>5</td>
+				<td>C</td>
+			</tr>
+			
+			<tr>
+				<td>Antônio Marcondes Lima</td>
+				<td>8</td>
+				<td>9</td>
+				<td>6</td>
+				<td>4</td>
+				<td>C</td>
+			</tr>
+			
+			<tr>
+				<td>Bruna Gomes</td>
+				<td>8</td>
+				<td>9</td>
+				<td>5</td>
+				<td>5</td>
+				<td>C</td>
+			</tr>
+
+			<tr>
+				<td>Cristiane de Souza Abreu</td>
+				<td>4</td>
+				<td>9</td>
+				<td>4</td>
+				<td>3</td>
+				<td>D</td>
+			</tr>
+
+			<tr>
+				<td>Cristiane Koehler</td>
+				<td>8</td>
+				<td>9</td>
+				<td>7</td>
+				<td>5</td>
+				<td>B</td>
+			</tr>
+
+			<tr>
+				<td>Daniele Guimarães Leal</td>
+				<td>8</td>
+				<td>9</td>
+				<td>7</td>
+				<td>5</td>
+				<td>C</td>
+			</tr>
+
+			<tr>
+				<td>Givaldo Batista Medeiros</td>
+				<td>8</td>
+				<td>9</td>
+				<td>5</td>
+				<td>5</td>
+				<td>C</td>
+			</tr>
+
+			<tr>
+				<td>Kelly Galarza</td>
+				<td>7</td>
+				<td>9</td>
+				<td>7</td>
+				<td>5</td>
+				<td>C</td>
+			</tr>
+
+			<tr>
+				<td>Liége Barbosa</td>
+				<td>8</td>
+				<td>9</td>
+				<td>8</td>
+				<td>5</td>
+				<td>B</td>
+			</tr>
+
+			<tr>
+				<td>Magali Inês Passini</td>
+				<td>8</td>
+				<td>9</td>
+				<td>6</td>
+				<td>3</td>
+				<td>C</td>
+			</tr>
+			<tr>
+				<td>Raul Araújo Neto</td>
+				<td>8</td>
+				<td>9</td>
+				<td>7</td>
+				<td>5</td>
+				<td>C</td>
+			</tr>
+
+			<tr>
+				<td>Tássia Priscila Fagundes</td>
+				<td>8</td>
+				<td>9</td>
+				<td>6</td>
+				<td>5</td>
+				<td>C</td>
+			</tr>
+		</table>
+	</div>
+
+</div>
+</body>
+</html>
---
+++ dashboard/das-student.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Das - Student</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/das-style-student.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+</body>
+</html>
