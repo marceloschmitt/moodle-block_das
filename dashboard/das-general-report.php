ec769ae9a3c79bce5f6abca23c5afd8fd0413339
patriciawalau <33928581+patriciawalau@users.noreply.github.com>
2018-10-31 16:55:40 -0300

    Update das-general-report.php

--- dashboard/das-general-report.php
+++ dashboard/das-general-report.php
@@ -78,262 +78,361 @@
 
 	<div class="das-table-select">
 		<table border="1" id="das-table-students-col">
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p class="das-title">Aluno</p></td>
 			</tr>
 
-			<tr><td>&nbsp;</td></tr>
+			<tr class="das-color-9"><td>&nbsp;</td></tr>
 
 			<tr>
-				<td><p>Anita Raquel da Silva</p></td>
+				<td class="das-color-orange"><p>Anita Raquel da Silva</p></td>
 			</tr>
 
 			<tr>
-				<td><p>Antônio Marcondes Lima</p></td>
+				<td class="das-color-orange"><p>Antônio Marcondes Lima</p></td>
 			</tr>
 
 			<tr>
-				<td><p>Bruna Gomes</p></td>
+				<td class="das-color-orange"><p>Bruna Gomes</p></td>
 			</tr>
 
 			<tr>
-				<td><p>Cristiane de Souza Abreu</p></td>
+				<td class="das-color-orange"><p>Cristiane de Souza Abreu</p></td>
 			</tr>
 
 			<tr>
-				<td><p>Daniela Guimarães Leal</p></td>
+				<td class="das-color-orange"><p>Daniela Guimarães Leal</p></td>
 			</tr>
 
 			<tr>
-				<td><p>Givaldo Batista Medeiros</p></td>
+				<td class="das-color-orange"><p>Givaldo Batista Medeiros</p></td>
 			</tr>
 
 			<tr>
-				<td><p>Kelly Galarza</p></td>
+				<td class="das-color-orange"><p>Kelly Galarza</p></td>
 			</tr>
 
 			<tr>
-				<td><p>Liêge Barbosa</p></td>
+				<td class="das-color-orange"><p>Liêge Barbosa</p></td>
 			</tr>
 
 			<tr>
-				<td><p>Magali Inês Passini</p></td>
+				<td class="das-color-orange"><p>Magali Inês Passini</p></td>
 			</tr>
 
 			<tr>
-				<td><p>Raul Araújo Neto</p></td>
+				<td class="das-color-orange"><p>Raul Araújo Neto</p></td>
 			</tr>
 
 			<tr>
-				<td><p>Tássia Priscila Fagundes</p></td>
+				<td class="das-color-orange"><p>Tássia Priscila Fagundes</p></td>
 			</tr>
 		</table>
 
 		<table border="1" id="das-students-table-col-notes">
 			<tr>
-				<th colspan="4"><p class="das-title">Integração de Mídias na Educação</p> </th>
-				<td><p class="das-title">Conceito</p></td>
+				<th class="das-color-9" colspan="4"><p class="das-title">Integração de Mídias na Educação</p> </th>
+				<td class="das-color-ccc"><p class="das-title">Conceito</p></td>
 			</tr>
 
 			<tr>
-				<td>A1</td>
-				<td>A2</td>
-				<td>A3</td>
-				<td>A4</td>
-				<td>&nbsp;</td>
+				<td class="das-color-ccc">A1</td>
+				<td class="das-color-ccc">A2</td>
+				<td class="das-color-ccc">A3</td>
+				<td class="das-color-ccc">A4</td>
+				<td class="das-color-9">&nbsp;</td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>6</p></td>
 				<td><p>9</p></td>
 				<td><p>4</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>6</p></td>
 				<td><p>4</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>5</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>4</p></td>
 				<td><p>9</p></td>
 				<td><p>4</p></td>
 				<td><p>3</p></td>
-				<td><p><b>D</b></p></td>
+				<td class="das-color-orange"><p><b>D</b></p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>7</p></td>
 				<td><p>5</p></td>
-				<td><p>B</p></td>
+				<td class="das-color-orange"><p>B</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>7</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc"> 
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>5</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>7</p></td>
 				<td><p>9</p></td>
 				<td><p>7</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>8</p></td>
 				<td><p>5</p></td>
-				<td><p>B</p></td>
+				<td class="das-color-orange"><p>B</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>6</p></td>
 				<td><p>3</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>6</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 		</table>
 
 		<table border="1" id="das-students-table-col-notes">
 			<tr>
-				<th colspan="4"><p class="das-title">Projeto e Produção de Material</p> </th>
-				<td><p class="das-title">Conceito</p></td>
+				<th colspan="4" class="das-color-9"><p class="das-title">Projeto e Produção de Material</p> </th>
+				<td class="das-color-ccc"><p class="das-title">Conceito</p></td>
 			</tr>
 
 			<tr>
-				<td>A1</td>
-				<td>A2</td>
-				<td>A3</td>
-				<td>A4</td>
-				<td>&nbsp;</td>
+				<td class="das-color-ccc">A1</td>
+				<td class="das-color-ccc">A2</td>
+				<td class="das-color-ccc">A3</td>
+				<td class="das-color-ccc">A4</td>
+				<td class="das-color-9">&nbsp;</td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>6</p></td>
 				<td><p>9</p></td>
 				<td><p>4</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>6</p></td>
 				<td><p>4</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>5</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>4</p></td>
 				<td><p>9</p></td>
 				<td><p>4</p></td>
 				<td><p>3</p></td>
-				<td><p><b>D</b></p></td>
+				<td class="das-color-orange"><p><b>D</b></p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>7</p></td>
 				<td><p>5</p></td>
-				<td><p>B</p></td>
+				<td class="das-color-orange"><p>B</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>7</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>5</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>7</p></td>
 				<td><p>9</p></td>
 				<td><p>7</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>8</p></td>
 				<td><p>5</p></td>
-				<td><p>B</p></td>
+				<td class="das-color-orange"><p>B</p></td>
 			</tr>
 
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>6</p></td>
 				<td><p>3</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
-			<tr>
+			<tr class="das-color-ccc">
 				<td><p>8</p></td>
 				<td><p>9</p></td>
 				<td><p>6</p></td>
 				<td><p>5</p></td>
-				<td><p>C</p></td>
+				<td class="das-color-orange"><p>C</p></td>
 			</tr>
+		</table>
+
+		<table border="1" id="das-table-notes">
+			<tr>
+				<td class="das-color-orange">Nota</td>
+				<td class="das-color-orange">Conceito</td>
+			</tr>
+
+			<tr class="das-color-ccc">
+				<td>10-9</td>
+				<td>A</td>
+			</tr>
+			
+			<tr class="das-color-ccc">
+				<td>8.9-7.5</td>
+				<td>B</td>
+			</tr>
+
+			<tr class="das-color-ccc">
+				<td>7.4-6</td>
+				<td>C</td>
+			</tr class="das-color-ccc">
+
+			<tr class="das-color-ccc">
+				<td>5.9-0.1</td>
+				<td>D</td>
+			</tr>
+
+			<tr class="das-color-ccc">
+				<td>0</td>
+				<td>FF</td>
+			</tr>
+
+
 		</table>
 
 	</div>
+
+	<div class="das-table-activities">
+		<table border="1" id="das-table-activities-names">
+			<tr>
+				<td colspan="2" class="das-color-orange"><p class="das-title">Integração de Mídias na Educação</p></td>
+			</tr>
+			<tr class="das-color-ccc">
+				<td>A1</td>
+				<td><p>Atividade 1 - Compreensão Inicial de uso de Mídias na Educação (Fórum 8)</p></td>
+			</tr>
+
+			<tr class="das-color-ccc">
+				<td>A2</td>
+				<td><p>Atividade 2 - Diagnóstico de uso de laboratórios de Informática, Tecnologias e Mídias na Instituição Escolar</p></td>
+			</tr>
+
+			<tr class="das-color-ccc">
+				<td>A3</td>
+				<td><p>Atividade 3 - Síntese Introdução à Midias na Educação</p></td>
+			</tr>
+
+			<tr class="das-color-ccc">
+				<td>A4</td>
+				<td><p>Atividade 4 - Criando Cineclub sobre Cultura Digital - Compartilhando resenhas e indicações de filme sobre o tema</p></td>
+			</tr>
+		</table>
+		
+		<table border="1" id="das-table-activities-names">
+			<tr class="das-color-ccc">
+				<td colspan="2" class="das-color-orange"><p class="das-title">Projeto e Produção de Material</p></td>
+			</tr>
+			<tr class="das-color-ccc">
+				<td>A1</td>
+				<td><p>Atividade 1 - Compreensão Inicial de uso de Mídias na Educação (Fórum 8)</p></td>
+			</tr>
+
+			<tr class="das-color-ccc">
+				<td>A2</td>
+				<td><p>Atividade 2 - Diagnóstico de uso de laboratórios de Informática, Tecnologias e Mídias na Instituição Escolar</p></td>
+			</tr>
+
+			<tr class="das-color-ccc">
+				<td>A3</td>
+				<td><p>Atividade 3 - Síntese Introdução à Midias na Educação</p></td>
+			</tr>
+
+			<tr class="das-color-ccc">
+				<td>A4</td>
+				<td><p>Atividade 4 - Criando Cineclub sobre Cultura Digital - Compartilhando resenhas e indicações de filme sobre o tema</p></td>
+			</tr>
+		</table>
+	</div>
+		
+	<div class="das-download-table">
+		<p style="margin-right: 2em;">Baixar todos os dados como tabela</p>
+		<select id="das-select-download">
+			<option>Valores Separados por Vírgula(.csv)</option>
+			<option>Microsoft Excel(.xlsx)</option>
+			<option>Tabela HTML</option>
+			<option>Javascript Object Notation(.json)</option>
+			<option>Open Document (.ods)</option>
+			</select>
+		<button id="das-download">Download</button>
+	</div>
+
 </div>
 </body>
 </html>
