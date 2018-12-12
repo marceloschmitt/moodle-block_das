9dd847fac14ca0dea42112e6458fdb894e525d69
patriciawalau <33928581+patriciawalau@users.noreply.github.com>
2018-12-12 14:50:45 -0200

    Update block_das.php

--- lang/pt_br/block_das.php
+++ lang/pt_br/block_das.php
@@ -25,7 +25,7 @@ $string['das'] = 'DAS - clique aqui';
 
 //second column.
 $string['anticipatedactivities'] = 'Atividades Antecipadas';
-$string['preventivenotice'] = 'Aviso Preventivo';
+$string['missingusers'] = 'Usuários ausentes';
 $string['remissionnotice'] = 'Aviso de Reenvio';
 $string['deliveredontime'] = 'Entregue no Prazo';
 $string['deliveredoutoftime'] = 'Entregue Fora do Prazo';
