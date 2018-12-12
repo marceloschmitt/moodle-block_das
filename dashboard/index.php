19b767868624729dafef8ef62c22b8979ee246cf
patriciawalau <33928581+patriciawalau@users.noreply.github.com>
2018-12-12 14:05:36 -0200

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -28,6 +28,7 @@ $courseusers = das_course_users($courseid);
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dash Fluxo de Caixas</title>
