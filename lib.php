cc2d876bc21e01af1a24f0a3334978db84ba3afd
patriciawalau <33928581+patriciawalau@users.noreply.github.com>
2018-12-12 13:43:14 -0200

    Update lib.php

--- lib.php
+++ lib.php
@@ -234,7 +234,7 @@ function das_print_student_list($courseusers, $students, $divid, $activityname, 
                 ?><div class="das-item-default-expansive <?php echo $divid; ?>" style="display: none">
                  <img class="das-user-small-image das-vertical-align" src="<?php echo $courseusers[$student['userid']]->pictureurl;?>" alt="User-Image">
                 <p class="das-vertical-align das-p-overflow"><?php echo $courseusers[$student['userid']]->fullname; ?></p>
-                <i class="icon fa fa-comment fa-fw " aria-hidden="true" title="Alternar menu de mensagens" aria-label="Alternar menu de mensagens"></i>
+                <i class="icon fa fa-comment fa-fw " aria-hidden="true" title="das-message"></i>
                 </div><?php
             }
 }
