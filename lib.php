7c12931a0be832809660d10d6348dc5e776b2136
Marcelo Augusto Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2019-04-11 09:10:17 -0300

    Update lib.php

--- lib.php
+++ lib.php
@@ -127,7 +127,7 @@ function das_print_missing_users($courseusers, $lowboundary, $highboundary=10000
         <p class="das-vertical-align das-p-overflow"><?php echo "$user->fullname - ";?>
 
         <a href="<?php echo $CFG->wwwroot;?>/message?id=i<?php echo $user->userid;?>">
-                    <img src="assets/img/comment-solid.png" alt="Message-Image">
+                    <img class="das-missing-user-days-white" src="assets/img/comment-solid.png" alt="Message-Image">
         </a>
         </p>
 
