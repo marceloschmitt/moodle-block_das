aa44c03a20994f143be39210f715ea9d7ac7cb8d
Marcelo Augusto Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2019-04-11 10:17:19 -0300

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -5,13 +5,24 @@ class block_das_edit_form extends block_edit_form {
     protected function specific_definition($mform) {
 
         // Section header title according to language file.
-        $mform->addElement('header', 'config_header', 'Teste');
+        $mform->addElement('header', 'config_header', 'Usuarios ausentes');
 
         // A sample string variable with a default value.
-        $mform->addElement('text', 'config_text', 'campo um');
-        $mform->setDefault('config_text', 'default value');
-        $mform->setType('config_text', PARAM_RAW);
+        $mform->addElement('text', 'config_beginoffirstgap', 'Inicio do primeiro intervalo');
+        $mform->setDefault('config_beginoffirstgap', '0');
+        $mform->setType('config_begindoffirstgap', PARAM_RAW);
 
+        $mform->addElement('text', 'config_beginofsecondgap', 'Inicio do segundoi intervalo');
+        $mform->setDefault('config_beginofsecondgap', '7');
+        $mform->setType('config_begindofsecondgap', PARAM_RAW);
+
+        $mform->addElement('text', 'config_beginofthirdgap', 'Inicio do terceiro intervalo');
+        $mform->setDefault('config_beginofthirdgap', '11');
+        $mform->setType('config_begindofthirdgap', PARAM_RAW);
+
+        $mform->addElement('text', 'config_beginofforthgap', 'Inicio do quarto intervalo');
+        $mform->setDefault('config_beginofforthgap', '60');
+        $mform->setType('config_begindofforthgap', PARAM_RAW);
     }
     }
     ?>
